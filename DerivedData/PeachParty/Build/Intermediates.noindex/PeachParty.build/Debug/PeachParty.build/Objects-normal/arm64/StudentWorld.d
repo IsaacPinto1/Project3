a2e51bac5f913ee32b46831a730a082ee544f3bf
Isaac Pinto <isaac.pinto2003@gmail.com>
2023-02-20 10:38:08 -0800

    Initial Commit

---
+++ DerivedData/PeachParty/Build/Intermediates.noindex/PeachParty.build/Debug/PeachParty.build/Objects-normal/arm64/StudentWorld.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/isaacpinto/Downloads/PeachParty\ 2/PeachParty/StudentWorld.cpp \
+  /Users/isaacpinto/Downloads/PeachParty\ 2/PeachParty/StudentWorld.h \
+  /Users/isaacpinto/Downloads/PeachParty\ 2/PeachParty/GameWorld.h \
+  /Users/isaacpinto/Downloads/PeachParty\ 2/PeachParty/GameConstants.h \
+  /Users/isaacpinto/Downloads/PeachParty\ 2/PeachParty/Board.h
