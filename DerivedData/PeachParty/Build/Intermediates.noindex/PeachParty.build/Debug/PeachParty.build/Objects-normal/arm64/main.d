b9218b94ba4faa9e895ac0b3a4a66de03183f8a1
Isaac Pinto <isaac.pinto2003@gmail.com>
2023-02-20 16:40:35 -0800

    untrack files

--- DerivedData/PeachParty/Build/Intermediates.noindex/PeachParty.build/Debug/PeachParty.build/Objects-normal/arm64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/isaacpinto/xCode/CS32/Projects/Project\ 3/PeachParty/main.cpp \
-  /Users/isaacpinto/xCode/CS32/Projects/Project\ 3/PeachParty/GameController.h \
-  /Users/isaacpinto/xCode/CS32/Projects/Project\ 3/PeachParty/SpriteManager.h \
-  /usr/X11/include/GL/freeglut.h /usr/X11/include/GL/freeglut_std.h \
-  /usr/X11/include/GL/freeglut_ext.h \
-  /usr/X11/include/GL/freeglut_ucall.h \
-  /Users/isaacpinto/xCode/CS32/Projects/Project\ 3/PeachParty/GameConstants.h
